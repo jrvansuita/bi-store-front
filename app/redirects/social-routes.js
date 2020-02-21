28790c4fd4fd34aa45cde632e8a6c1a5da2e6d04
juniorvansuita <Vansuita1324>
2020-02-20 22:59:58 -0300

    tste

--- app/redirects/social-routes.js
+++ app/redirects/social-routes.js
@@ -2,8 +2,10 @@ const Routes = require('../redirects/controller/routes.js');
 
 module.exports = class SocialRoutes extends Routes {
   attach(){
-    this._get('/social-floating-widget', (req, res) => { 
+    this._get('/social-floating-widget', (req, res) => {
+      console.time('social-floating-widget');
       res.render('social/social-floating-widget');
+      console.timeEnd('social-floating-widget');
     });
   }
 }
