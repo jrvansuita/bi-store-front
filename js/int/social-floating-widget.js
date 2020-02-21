89a66ab05dd21459352f603659ec10b29872c410
juniorvansuita <Vansuita1324>
2020-02-20 21:57:28 -0300

    ef

--- js/int/social-floating-widget.js
+++ js/int/social-floating-widget.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function(){
   jQuery('.sfmw-action').not('.sfmw-mini-action').click(function() {
-    $('.sfmw-action').hide();
-    $('.sfmw-close').show().css('display', 'block');
+    jQuery('.sfmw-action').hide();
+    jQuery('.sfmw-close').show().css('display', 'block');
 
-    $(this).siblings('.sfmw-mini-action').show().css('display', 'block');
+    jQuery(this).siblings('.sfmw-mini-action').show().css('display', 'block');
   });
 
   jQuery('.sfmw-close').click(function() {
-    $('.sfmw-action').show();
-    $('.sfmw-close').hide();
+    jQuery('.sfmw-action').show();
+    jQuery('.sfmw-close').hide();
 
-    $('.sfmw-mini-action').hide();
+    jQuery('.sfmw-mini-action').hide();
   });
 });
