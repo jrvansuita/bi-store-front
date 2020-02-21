54aa29f216b5c8fc61adbcf2352daf1ce6eac430
juniorvansuita <Vansuita1324>
2020-02-21 00:02:36 -0300

    super cache

--- app/redirects/controller/routes.js
+++ app/redirects/controller/routes.js
@@ -1,10 +1,28 @@
-
 module.exports = class Routes {
 
   constructor(app){
     this.app = app;
+    this.cache = {};
   }
 
+  _setDefaultHeaders(res){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Cache-Control', 'public, max-age=31557600');
+  }
+
+  _setCacheOption(res){
+    res.renderAndCache = (path) => {
+      if (this.cache[path]){
+        res.send(this.cache[path]);
+      }else{
+        res.render(path, (err, html) => {
+          this.cache[path] = html;
+          res.send(html);
+        });
+      }
+    }
+  }
 
   _routes(){
     return this.app._router.stack;
@@ -12,12 +30,11 @@ module.exports = class Routes {
 
   _get(paths, callback){
     this.app.get(paths, (req, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      this._setDefaultHeaders(res);
+      this._setCacheOption(res);
       callback(req, res);
     });
   }
-
 
   _post(paths, callback){
     this.app.post(paths, (req, res) => {
