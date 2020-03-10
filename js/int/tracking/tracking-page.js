badd13f202645885de368c4fcdbca2d85c180cad
Junior Vansuita <jrvansuita@Dracon.local>
2020-03-10 15:22:04 -0300

    urls

--- js/int/tracking/tracking-page.js
+++ js/int/tracking/tracking-page.js
@@ -2,7 +2,7 @@ jQuery(document).ready(() => {
   jQuery('body').append(jQuery('<div>').load("__host/social-floating-widget"));
 
   new SideMenu(jQuery('.content'))
-  .addItem('Rastrear', 'loupe-w', '/tracking-sale')
+  .addItem('Rastrear', 'loupe-w', '__host/tracking-sale')
   .addDivider('Dúvidas Frequentes')
   .addItem('Análise Fiscal', 'check-w',  '__templateUrl59127298')
   .addItem('Número do Pedido?', 'box-w', '__templateUrl61668082')
@@ -11,6 +11,6 @@ jQuery(document).ready(() => {
   .build()
   .show();
 
-  jQuery('.content').load('/tracking-sale');
+  jQuery('.content').load('__host/tracking-sale');
 
 });
