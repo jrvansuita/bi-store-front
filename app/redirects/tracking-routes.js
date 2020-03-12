12f1c16529cc3b5038123d1251db69a4190c11b2
Junior Vansuita <jrvansuita@Dracon.local>
2020-03-12 12:45:37 -0300

    some changes

--- app/redirects/tracking-routes.js
+++ app/redirects/tracking-routes.js
@@ -1,5 +1,5 @@
 const Routes = require('../redirects/controller/routes.js');
-
+const fs = require('fs');
 
 module.exports = class TrackingRoutes extends Routes {
   attach(){
@@ -12,11 +12,22 @@ module.exports = class TrackingRoutes extends Routes {
     });
 
     this._get('/tracking-template', (req, res) => {
+      //teste      http://localhost:4000/tracking-template?id=61723810
+      var url = Params.hawkUrl() + '/templates-viewer?id=' + (req.query.id || '');
 
-      Params.hawkUrl() + '/templates-viewer?id=' + (id || '');
 
-      res.renderAndCache('tracking/pages/tracking-sale', {__trackingUrl: Params.trackingUrl()});
+
+
+      fs.readFile(url, function (err, html) {
+        console.log(err);
+        console.log(html);
+        res.send(html);
+      });
+
+
+
+
+      //res.renderAndCache('tracking/pages/tracking-sale', {__trackingUrl: Params.trackingUrl()});
     });
   }
 }
- 
