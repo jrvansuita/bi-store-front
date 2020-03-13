efd6fb64a798c0550ce36d2bdac866d1c2c2b2c5
Jaison <Jaison@iMac-de-Desenvolvimento.local>
2020-03-13 08:25:06 -0300

    fix

--- js/int/tracking/tracking-page.js
+++ js/int/tracking/tracking-page.js
@@ -2,14 +2,14 @@ jQuery(document).ready(() => {
   jQuery('body').append(jQuery('<div>').load("__host/social-floating-widget"));
 
   var show = new Util().deviceIsDesktop();
-  
+
   new SideMenu(jQuery('.content'))
   .addItem('Rastrear', 'loupe-w', '__host/tracking-sale')
   .addDivider('Dúvidas Frequentes')
-  .addItem('Análise Fiscal', 'check-w',  '__templateUrl59127298')
-  .addItem('Número do Pedido?', 'box-w', '__templateUrl61668082')
-  .addItem('Endereço Insuficiente', 'map-w', '__templateUrl61723810')
-  .addItem('Central de Atendimento', 'headset-w',  '__templateUrl61773960')
+  .addItem('Análise Fiscal', 'check-w',  '/template-viwer?id=59127298')
+  .addItem('Número do Pedido?', 'box-w', '/template-viwer?id=61668082')
+  .addItem('Endereço Insuficiente', 'map-w', '/template-viwer?id=61723810')
+  .addItem('Central de Atendimento', 'headset-w',  '/template-viwer?id=61773960')
   .build()
   .show(show);
 
