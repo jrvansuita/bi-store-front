dac7bb273ff839a0adb8b3938f3deb6faad85992
Jaison <Jaison@iMac-de-Desenvolvimento.local>
2020-03-13 08:33:56 -0300

    f

--- js/int/tracking/tracking-page.js
+++ js/int/tracking/tracking-page.js
@@ -6,13 +6,14 @@ jQuery(document).ready(() => {
   new SideMenu(jQuery('.content'))
   .addItem('Rastrear', 'loupe-w', '__host/tracking-sale')
   .addDivider('Dúvidas Frequentes')
-  .addItem('Análise Fiscal', 'check-w',  '/template-viwer?id=59127298')
-  .addItem('Número do Pedido?', 'box-w', '/template-viwer?id=61668082')
-  .addItem('Endereço Insuficiente', 'map-w', '/template-viwer?id=61723810')
-  .addItem('Central de Atendimento', 'headset-w',  '/template-viwer?id=61773960')
+  .addItem('Análise Fiscal', 'check-w',  '__host/template-viwer?id=59127298')
+  .addItem('Número do Pedido?', 'box-w', '__host/template-viwer?id=61668082')
+  .addItem('Endereço Insuficiente', 'map-w', '__host/template-viwer?id=61723810')
+  .addItem('Central de Atendimento', 'headset-w',  '__host/template-viwer?id=61773960')
   .build()
   .show(show);
 
   jQuery('.content').load('__host/tracking-sale');
 
+  console.log('__host');
 });
