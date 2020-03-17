3f97a2a0d5b74cc8b16fa4119c6ac29b0016223c
Junior Vansuita <jrvansuita@Dracon.local>
2020-03-17 08:48:14 -0300

    fix

--- js/int/util/file-upload-dialog.js
+++ js/int/util/file-upload-dialog.js
@@ -1,34 +1,48 @@
 class FileUploadDialog{
   constructor(){
-    this.holder = $('<div>').addClass('fudmb');
-    this.modal = $('<div>').addClass('fudm');
-    this.holder.append(this.modal);
-
     this._build();
   }
 
   _build(){
-    var box = $('<div>').addClass('fudb');
-    var ico = $('<div>').addClass('fudih').append($('<img>').attr('src', '__host/img/pic-folder-c.png'));
-    var inner = $('<div>').addClass('fudi');
+    this.holder = jQuery('<div>').addClass('fudmb');
+    this.modal = jQuery('<div>').addClass('fudm');
+    this.holder.append(this.modal);
+
+    var box = jQuery('<div>').addClass('fudb');
+    var ico = jQuery('<div>').addClass('fudih').append(jQuery('<img>').attr('src', '__host/img/pic-folder-c.png'));
+    var inner = jQuery('<div>').addClass('fudi');
     box.append(ico, inner);
     this.modal.append(box);
 
-    this.maintitle = $('<span>').addClass('fudt');
-    this.subTitle = $('<span>').addClass('fudst').text('Clique aqui para selecionar uma foto do seu computador.');
-    this.footTitle = $('<span>').addClass('fudft').text('Suporta: *.png, *.jpe, *.jpeg');
+    this.maintitle = jQuery('<span>').addClass('fudt');
+    this.subTitle = jQuery('<span>').addClass('fudst').text('Clique aqui para selecionar uma foto do seu computador.');
+    this.footTitle = jQuery('<span>').addClass('fudft').text('Suporta: *.png, *.jpe, *.jpeg');
 
     inner.append(this.maintitle, this.subTitle, this.footTitle);
   }
 
   title(title){
-    this.maintitle.text(title);
+    this.titleStr = title;
     return this;
   }
 
-  show(){
-    $('body').append(this.holder);
-    this.holder.fadeIn(400);
+  _bind(){
+    this.maintitle.text(this.titleStr);
+    this.holder.click(() => {
+      this.holder.fadeOut(200,()=>{
+        this.holder.remove();
+      });
+    });
+
+    this.modal.click((e) => {
+      e.stopPropagation();
+    });
   }
 
+  show(){
+    this._bind();
+
+    jQuery('body').append(this.holder);
+    this.holder.fadeIn('slow');
+  }
 }
